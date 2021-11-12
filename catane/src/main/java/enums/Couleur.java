d14b32ac5372caa9b6b3e37b8b3a4bc4f880048e
jadecrtl <jadecortial@gmail.com>
2021-11-12 17:35:45 +0100

    créations de classes énumérations

---
+++ catane/src/main/java/enums/Couleur.java
@@ -0,0 +1,5 @@
+package enums;
+
+public enum Couleur {
+    BLEU, BLANC, ROUGE, VERT;
+}
---
+++ catane/src/main/java/enums/TypeTerrain.java
@@ -0,0 +1,5 @@
+package enums;
+
+public enum TypeTerrain {
+    CHAMPS, PRES, FORETS, MONTAGNES, COLLINES, DESERT;
+}
