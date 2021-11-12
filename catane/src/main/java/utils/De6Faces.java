e9baed02cf63bbf93ad1651ad244b57a7d881c32
parismollo <parismollochristondis@gmail.com>
2021-11-12 17:58:10 +0100

    (test+class) add De6Faces test et classe

--- catane/src/main/java/utils/De6Faces.java
+++ catane/src/main/java/utils/De6Faces.java
@@ -1,5 +1,20 @@
 package utils;
+import java.util.Random;
 
 public class De6Faces {
-    // [TODO]
+    int nbFaces = 6;
+    private int valeurDe;
+    
+    De6Faces() {
+        this.lanceDe();
+    }
+
+    public int getValeurDe() {
+        return this.valeurDe;
+    }
+
+    public void lanceDe() {
+        Random r = new Random();
+        this.valeurDe = 1 + r.nextInt(nbFaces);
+    }
 }
--- catane/src/test/java/utils/De6facesUTest.java
+++ catane/src/test/java/utils/De6facesUTest.java
@@ -1,5 +1,20 @@
 package utils;
+// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.assertj.core.api.Assertions.*;
 
 public class De6facesUTest {
-    
+
+    @DisplayName("Teste du de de 6 faces")
+    void lanceDe6Faces() {
+        De6Faces d1 = new De6Faces();
+        int i = 0;
+
+        while (i<6000) {
+            d1.lanceDe();
+            assertThat(d1.getValeurDe()).isBetween(1, 6);
+            i++;
+        }
+    }
+
 }
