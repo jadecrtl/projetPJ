9098ba163a0c09780969731ce8e8f67c61ddffa9
jadecrtl <jadecortial@gmail.com>
2021-11-15 21:45:13 +0100

    On remplace les nombres magiques par des constantes

--- catane/src/main/java/principal/Jeu.java
+++ catane/src/main/java/principal/Jeu.java
@@ -4,13 +4,16 @@ import java.util.List;
 public class Jeu {
     
     private final List<Joueur> joueurs;
-    
+    public static final int VICTOIRE = 10;
+    public static final int JOUEURSMIN = 3;
+    public static final int JOUEURSMAX = 4;
+
 
     public Jeu(List<Joueur> joueurs){
         if (joueurs == null) {
             throw new IllegalArgumentException("On ne joue pas si pas de joueurs!");            
         }
-        if (!(joueurs.size() == 3 || joueurs.size() == 4)) {
+        if (joueurs.size() < JOUEURSMIN || joueurs.size() > JOUEURSMAX) {
             throw new IllegalArgumentException("Catan se joue a trois ou quatres joueurs!!!");
         }
         this.joueurs = joueurs;
@@ -19,7 +22,7 @@ public class Jeu {
     public boolean isJeuTermine() {
         int nbJoueurs = joueurs.size();
         for (int i = 0; i < nbJoueurs ; i++) {
-            if (joueurs.get(i).getPointVictoire() >= 10) {
+            if (joueurs.get(i).getPointVictoire() >= VICTOIRE) {
                 return true;
             }
         }
--- catane/src/test/java/principal/Jeu02FinPartieUTest.java
+++ catane/src/test/java/principal/Jeu02FinPartieUTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Jeu02FinPartieUTest {
 
     @Test
-    public void creationJeuTroisJoueurs() {
+    public void victoireJeuTroisJoueurs() {
         Joueur j1 = new Joueur("j1", 19, enums.TypeJoueur.HUMAIN, enums.Couleur.ROUGE);
         Joueur j2 = new Joueur("j2", 19, enums.TypeJoueur.HUMAIN, enums.Couleur.BLANC);
         Joueur j3 = new Joueur("j3", 19, enums.TypeJoueur.HUMAIN, enums.Couleur.BLEU);
@@ -28,6 +28,27 @@ public class Jeu02FinPartieUTest {
         assertThat(maPartie.isJeuTermine()).isTrue();
     }
 
+    @Test
+    public void victoireJeuQuatreJoueurs() {
+        Joueur j1 = new Joueur("j1", 19, enums.TypeJoueur.HUMAIN, enums.Couleur.ROUGE);
+        Joueur j2 = new Joueur("j2", 19, enums.TypeJoueur.HUMAIN, enums.Couleur.BLANC);
+        Joueur j3 = new Joueur("j3", 19, enums.TypeJoueur.HUMAIN, enums.Couleur.BLEU);
+        Joueur j4 = new Joueur("j4", 19, enums.TypeJoueur.HUMAIN, enums.Couleur.VERT);
+        List<Joueur> joueurs = new LinkedList<Joueur>();
+        joueurs.add(j1);
+        joueurs.add(j2); 
+        joueurs.add(j3);
+        joueurs.add(j4);        
+        Jeu maPartie = new Jeu(joueurs);
+        assertThat(maPartie.isJeuTermine()).isFalse();
+        j1.ajouterPointVictoire(5);
+        j2.ajouterPointVictoire(8);
+        j3.ajouterPointVictoire(2);
+        j4.ajouterPointVictoire(0);
+        assertThat(maPartie.isJeuTermine()).isFalse();
+        j1.ajouterPointVictoire(6);
+        assertThat(maPartie.isJeuTermine()).isTrue();
+    }
 
 
 
