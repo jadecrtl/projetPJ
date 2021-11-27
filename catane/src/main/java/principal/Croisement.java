fc7364f318660e9e148b37fb0ffbc3c1239676b8
jadecrtl <jadecortial@gmail.com>
2021-11-27 11:02:22 +0100

    croisement peut etre detenu par un joueur comme une ville ou colonie

--- catane/src/main/java/principal/Croisement.java
+++ catane/src/main/java/principal/Croisement.java
@@ -1,11 +1,16 @@
 package principal;
 
 import java.util.List;
+
+import enums.TypeCroisement;
+
 import java.util.LinkedList;
 
 public class Croisement {
     private int idCroisement;
     List<Integer> tuilesVoisines;
+    private TypeCroisement typeCroisement;
+    private Joueur proprietaire;
 
     public Croisement(int idCroisement) {
         if (idCroisement < 0) {
@@ -13,6 +18,26 @@ public class Croisement {
         }
         this.idCroisement = idCroisement;
         tuilesVoisines = new LinkedList<Integer>();
+    }
+
+    public Joueur getProprietaire() {
+        return proprietaire;
+    }
+
+    public void setProprietaire(Joueur proprietaire, TypeCroisement typeCroisement) {
+        if (proprietaire == null || typeCroisement == null) {
+            throw new IllegalArgumentException("Il faut assigner le proprietaire et le type de croisement.");
+        }
+        this.proprietaire = proprietaire;
+        this.typeCroisement = typeCroisement;
+    }
+
+    public TypeCroisement getTypeCroisement() {
+        return typeCroisement;
+    }
+
+    public void setTypeCroisement(TypeCroisement typeCroisement) {
+        this.typeCroisement = typeCroisement;
     }
 
     public int getIdCroisement() {
---
+++ catane/src/test/java/principal/Croisement01AssignationUTest.java
@@ -0,0 +1,39 @@
+package principal;
+
+import org.junit.jupiter.api.Test;
+
+import enums.Couleur;
+import enums.TypeCroisement;
+import enums.TypeJoueur;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class Croisement01AssignationUTest {
+    
+    @Test
+    public void croisementProprietaireNull() {
+        Croisement c1 = new Croisement(0);
+        assertThatThrownBy(() -> c1.setProprietaire(null,null)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void assignationProprioEtColonieSurCroisement() {
+        Croisement c2 = new Croisement(0);
+        Joueur j2 = new Joueur("j2", 19, TypeJoueur.HUMAIN, Couleur.BLANC);
+        c2.setProprietaire(j2, TypeCroisement.COLONIE);
+        assertThat(c2.getProprietaire()).isEqualTo(j2);
+        assertThat(c2.getTypeCroisement()).isEqualTo(TypeCroisement.COLONIE);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
