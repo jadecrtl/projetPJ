5e90b730e6f518022c35ac181d52180b3c02c469
Paris Mollo <parismollochristondis@gmail.com>
2021-11-29 00:51:13 +0100

    minor updates on AireDeJeu View

--- catane/src/main/java/gui/Gui.java
+++ catane/src/main/java/gui/Gui.java
@@ -1,9 +1,14 @@
 package gui;
 import gui.views.BoardView;
 
+
+/* 
+Gui main class for testing. (Temporary)
+*/
+
 public class Gui {
     public static void main(String[] args) {
-        new BoardView();
+        new BoardView(500, 500);
     }
     
 }
--- catane/src/main/java/gui/views/BoardView.java
+++ catane/src/main/java/gui/views/BoardView.java
@@ -4,32 +4,32 @@ import javax.swing.JFrame;
 import principal.AireDeJeu;
 
 /* 
-Board view.
+Board view for testing. (Temporary)
 */
 
 public class BoardView extends JFrame{
-    private AireDeJeuView aireDeJeuView;
+    private final String boardName;
+    private final int height;
+    private final int width;
 
-    public BoardView() {
-        super("Board");
+    public BoardView(int height, int width) {
+        this.boardName = "Board";
+        this.height = height;
+        this.width = width;
 
         AireDeJeu aireDeJeu = new AireDeJeu(5, 5);
         AireDeJeuView aireDeJeuView = new AireDeJeuView(aireDeJeu);
 
-        this.setAireDeJeuView(aireDeJeuView);
+        setBoardView(aireDeJeuView);
+    }
 
+    private void setBoardView(AireDeJeuView aireDeJeuView) {
+        setTitle(this.boardName);
         add(aireDeJeuView);
-        setSize(500, 500);
+        setSize(this.width, this.height);
+
         setResizable(true);
         setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-
-    public AireDeJeuView getAireDeJeuView() {
-        return aireDeJeuView;
-    }
-
-    public void setAireDeJeuView(AireDeJeuView aireDeJeuView) {
-        this.aireDeJeuView = aireDeJeuView;
-    }
-
 }
