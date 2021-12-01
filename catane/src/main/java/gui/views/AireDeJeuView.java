4ce6b6b94b62d705fade08cc79e6dfff2af7f071
Paris Mollo <parismollochristondis@gmail.com>
2021-12-01 21:55:14 +0100

    (gui) update AireDeJeuView + Tests

--- catane/src/main/java/gui/views/AireDeJeuView.java
+++ catane/src/main/java/gui/views/AireDeJeuView.java
@@ -1,6 +1,8 @@
 package gui.views;
 
 import javax.swing.*;
+
+import gui.controllers.AireDeJeuController;
 import principal.AireDeJeu;
 
 import java.awt.*;
@@ -8,19 +10,44 @@ import java.awt.*;
 Aire de Jeu view.
 */
 public class AireDeJeuView extends JPanel{
+    // Model 
+    private AireDeJeu aireDeJeu;
+    // Controleur
+    private AireDeJeuController aireDeJeuController;
+    // Layout
     private GridLayout cataneGrid;
+
     public AireDeJeuView(AireDeJeu aireDeJeu) {
-        this.cataneGrid = new GridLayout(aireDeJeu.getHorizontale(), aireDeJeu.getVerticale());
+        // Set View attributes
+        this.aireDeJeu = aireDeJeu;
+        // Creation du Grid
+        this.cataneGrid = new GridLayout(this.aireDeJeu.getHorizontale(), this.aireDeJeu.getVerticale());
+        // Mise en place du Layout
         this.setLayout(cataneGrid);
-        this.addTuiles(aireDeJeu);
+        // Ajout des tuilles
+        this.addTuiles();
     }
 
-    public void addTuiles(AireDeJeu aireDeJeu) {
+    // Méthodes d'ajout de contenu dans JPanel
+    public void addTuiles() {
         setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
-        for (int i =0; i<(aireDeJeu.getVerticale()*aireDeJeu.getHorizontale()); i++){
+        for (int i =0; i<(this.aireDeJeu.getVerticale()*this.aireDeJeu.getHorizontale()); i++){
             JLabel tuile = new JLabel("Tuile: "+i);  // Todo: add tuile component.
             tuile.setBorder(BorderFactory.createLineBorder(Color.BLACK));
             add(tuile);
         }
     }
+
+    // Setters
+    public void setControleur(AireDeJeuController aireDeJeuController) {
+        this.aireDeJeuController = aireDeJeuController;
+    }
+
+    public AireDeJeuController getControleur() {
+        return this.aireDeJeuController;
+    }
+
+    // public void setModel(AireDeJeu aireDeJeu) {
+    //     this.aireDeJeu = aireDeJeu;
+    // }
 }
--- catane/src/main/java/gui/views/BoardView.java
+++ catane/src/main/java/gui/views/BoardView.java
@@ -1,35 +1,34 @@
 package gui.views;
-
 import javax.swing.JFrame;
+
+import gui.controllers.AireDeJeuController;
 import principal.AireDeJeu;
 
-/* 
-Board view for testing. (Temporary)
-*/
-
 public class BoardView extends JFrame{
-    private final String boardName;
+    private final String boardName = "Catan Board";
     private final int height;
     private final int width;
 
-    public BoardView(int height, int width) {
-        this.boardName = "Board";
+    public BoardView(int height, int width, AireDeJeu aireDeJeu) {
+
+        // JFrame config
         this.height = height;
         this.width = width;
-
-        AireDeJeu aireDeJeu = new AireDeJeu(5, 5);
-        AireDeJeuView aireDeJeuView = new AireDeJeuView(aireDeJeu);
-
-        setBoardView(aireDeJeuView);
-    }
-
-    private void setBoardView(AireDeJeuView aireDeJeuView) {
         setTitle(this.boardName);
-        add(aireDeJeuView);
         setSize(this.width, this.height);
-
         setResizable(true);
-        setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Set JPanels
+        AireDeJeuView aireDeJeuView = new AireDeJeuView(aireDeJeu);
+        AireDeJeuController aireDeJeuController = new AireDeJeuController();
+
+        // Set JPanels Dependecies
+        // aireDeJeuView.setModel(aireDeJeu);
+        aireDeJeuView.setControleur(aireDeJeuController);
+        
+
+        // Add content to view
+        add(aireDeJeuView);
     }
 }
