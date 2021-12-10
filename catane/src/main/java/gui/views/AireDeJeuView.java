47d45c6ed61871a957a183664a00dd5457e1f631
Paris Mollo <parismollochristondis@gmail.com>
2021-12-10 18:57:04 +0100

    feat: add new atttributes to TuileView

--- catane/src/main/java/gui/views/AireDeJeuView.java
+++ catane/src/main/java/gui/views/AireDeJeuView.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 import gui.controllers.AireDeJeuController;
 import principal.AireDeJeu;
 import principal.Tuile;
-import gui.views.*;
+// import gui.views.*;
 import enums.*;
 
 import java.awt.*;
@@ -35,13 +35,14 @@ public class AireDeJeuView extends JPanel{
     public void addTuiles() {
         setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
         for (int i =0; i<(this.aireDeJeu.getVerticale()*this.aireDeJeu.getHorizontale()); i++){
-            // JLabel tuile = new JLabel("Tuile: "+i);  // Todo: add tuile component.
-            Tuile tuileModel = new Tuile(2);
+            // FIXME: Use a proper method from backend
+            Tuile tuileModel = new Tuile(2);            
             tuileModel.setJeton(5);
             tuileModel.setTerrain(Terrain.DESERT);
             JPanel tuile = new TuilesView(tuileModel);
             tuile.setBorder(BorderFactory.createLineBorder(Color.BLACK));
             add(tuile);
+            // ENDFIXME
         }
     }
 
@@ -53,8 +54,4 @@ public class AireDeJeuView extends JPanel{
     public AireDeJeuController getControleur() {
         return this.aireDeJeuController;
     }
-
-    // public void setModel(AireDeJeu aireDeJeu) {
-    //     this.aireDeJeu = aireDeJeu;
-    // }
 }
--- catane/src/main/java/gui/views/TuilesView.java
+++ catane/src/main/java/gui/views/TuilesView.java
@@ -1,35 +1,32 @@
 package gui.views;
-
-import java.util.List;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
 
 import gui.controllers.TuilesController;
 import principal.Tuile;
 
 public class TuilesView extends JPanel {
-    private JLabel tuileBackground;
+
     private JLabel jetonLabel = new JLabel();
-    // private List<JButton> croisements;
+    private JLabel terrain = new JLabel();
+
     private Tuile tuile;
+    
     private TuilesController tuilleController;
-    private BorderLayout tuileLayout; 
+    private BorderLayout tuileLayout = new BorderLayout();
 
     public TuilesView(Tuile tuile) {
         this.tuile = tuile;
+        this.setLayout(tuileLayout);
+
         this.jetonLabel.setText(String.valueOf(this.tuile.getJeton()));
-        this.tuileLayout = new BorderLayout();
-        // try {
-        //     // this.tuileBackground = new JLabel(new ImageIcon(ImageIO.read(new File("src/static/backgroundRock.jpg"))));
-        //     add(tuileBackground);
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
-        add(this.jetonLabel, BorderLayout.CENTER);
+        this.jetonLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        this.terrain.setText(String.valueOf(this.tuile.getTerrain()));
+        this.terrain.setHorizontalAlignment(JLabel.CENTER);
+
+        add(this.jetonLabel, BorderLayout.NORTH);
+        add(this.terrain, BorderLayout.SOUTH);
     }
 
 }
