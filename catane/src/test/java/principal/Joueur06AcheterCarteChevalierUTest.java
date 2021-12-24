908cce8b62807d60b7f4aebee5edd5571008eb12
jadecrtl <jadecortial@gmail.com>
2021-12-24 15:41:55 +0100

    mise en commentaires des choses inutiles

--- catane/src/test/java/principal/Joueur06AcheterCarteChevalierUTest.java
+++ catane/src/test/java/principal/Joueur06AcheterCarteChevalierUTest.java
@@ -3,13 +3,13 @@ package principal;
 import org.junit.jupiter.api.Test;
 
 import enums.Production;
-import utils.De6Faces;
+//import utils.De6Faces;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.OptionalLong;
+//import java.util.OptionalLong;
 
 public class Joueur06AcheterCarteChevalierUTest {
 
@@ -19,12 +19,12 @@ public class Joueur06AcheterCarteChevalierUTest {
         Joueur j2 = new Joueur("j2", 19, enums.TypeJoueur.HUMAIN, enums.Couleur.BLANC);
         Joueur j3 = new Joueur("j3", 19, enums.TypeJoueur.HUMAIN, enums.Couleur.BLEU);
         List<Joueur> joueurs = new LinkedList<Joueur>();
-        De6Faces de1 = new De6Faces();
-        De6Faces de2 = new De6Faces();
+        //De6Faces de1 = new De6Faces();
+        //De6Faces de2 = new De6Faces();
         joueurs.add(j1);
         joueurs.add(j2); 
         joueurs.add(j3);
-        Jeu jeu = new Jeu(joueurs, 5, 3, de1, de2, OptionalLong.of(123));    
+        //Jeu jeu = new Jeu(joueurs, 5, 3, de1, de2, OptionalLong.of(123));    
         assertThat(j1.getInventaireChevalier()).isEqualTo(0);
         j1.ajouterRessources(1, Production.MINERAI);
         j1.ajouterRessources(1, Production.BLE);
